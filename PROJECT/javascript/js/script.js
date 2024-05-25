778183a2bf561ef20aed205ca0d4fccbdd8fc4c0
trungnguyenngoc123 <137521550+trungnguyenngoc123@users.noreply.github.com>
2024-05-25 13:35:12 +0700

    Delete PROJECT directory

--- PROJECT/javascript/js/script.js
+++
@@ -1,9 +0,0 @@
-const addressbtn = document.querySelector('#address-form');
-const addressclose = document.querySelector('#address-close');
-// console.log(addressbtn);
-addressbtn.addEventListener("click", function(){//add event
-    document.querySelector('.address-form').style.display = 'flex';
-})
-addressclose.addEventListener("click", function(){//add event
-    document.querySelector('.address-form').style.display = 'none';
-})

f70e1daacc0b5dc5ccce1f6e774419e58b43dbe4
Pham Khanh Nhan <78801337+kleqing@users.noreply.github.com>
2024-06-17 21:31:43 +0700

    update model class

---
+++ FINAL_PROJECT/src/java/Model/User.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author hotaru
+ */
+public class User {
+    private int userID;
+    private String fullName;
+    private String email;
+    private int phone;
+    private String address;
+    private int roleID;
+    private String password;
+
+    public User() {
+    }
+
+    public User(int userID, String fullName, String email, int phone, String address, int roleID, String password) {
+        this.userID = userID;
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.roleID = roleID;
+        this.password = password;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getRoleID() {
+        return roleID;
+    }
+
+    public void setRoleID(int roleID) {
+        this.roleID = roleID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "userID=" + userID + ", fullName=" + fullName + ", email=" + email + ", phone=" + phone + ", address=" + address + ", roleID=" + roleID + ", password=" + password + '}';
+    }
+    
+}
